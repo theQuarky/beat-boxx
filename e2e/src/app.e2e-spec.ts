a22ed5fd01c55c5eb4bea161e8f031eeffb31ea7
theQuarky <ranahiren27@gmail.com>
2025-12-09 16:06:10 +0530

    Rename project to BeatBoxx; fix build and add pad tactile improvements

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tap-music app is running!');
+    expect(page.getTitleText()).toEqual('BeatBoxx app is running!');
   });
 
   afterEach(async () => {
--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tap-music'));
+app.use(express.static(__dirname + '/dist/BeatBoxx'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/tap-music/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/BeatBoxx/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
